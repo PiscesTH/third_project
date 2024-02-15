281a47076cc9fd4a7c55083d13b3de60d65fe8fe
TH <kareme007@gmail.com>
2024-02-15 18:10:39 +0900

    24.02.15. email

--- src/main/java/com/baby/babycareproductsshop/mail/ConsoleMailService.java
+++ src/main/java/com/baby/babycareproductsshop/mail/ConsoleMailService.java
@@ -4,16 +4,12 @@ import com.baby.babycareproductsshop.mail.model.EmailMessageDto;
 import com.baby.babycareproductsshop.mail.model.MailSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ConsoleMailService implements MailSender {
-    private final JavaMailSender javaMailSender;
-
     @Override
     public void send(EmailMessageDto emailMessageDto) {
         log.info("email - to : {}, subject : {}, message : {}",
